adbdd87537a45983fd339f2c7b4d5e71b07b7d6c
octopus <912554887@qq.com>
2021-10-03 15:46:41 +0800

    add bcos-boostssl

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -4,6 +4,12 @@ hunter_config(bcos-crypto
 	SHA1 4a1649e7095f5db58a5ae0671b2278bcccc25f1d
 )
 
+hunter_config(bcos-boostssl
+	VERSION 3.0.0-local
+	URL "https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/cb2ca607a30012220f5d460471565285833d5da3.tar.gz"
+	SHA1 d1a89d9f96e659caa6557dc91178f1fe810e442a
+)
+
 hunter_config(bcos-framework VERSION 3.0.0-33486b94
 	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/5975c8db063bf97656f4e0a5ba5aa729d7c24977.tar.gz
 	SHA1 88c3c0a9282d357607f66b6e2860897a250bd61f
